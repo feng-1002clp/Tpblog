8fdf6342bc2be45d68ab01616f3b7ad73e463378
Fzl <f18970254621@163.com>
2020-10-26 10:01:20 +0800

    123

---
+++ git.sh
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+#!/bin/bash
+git add -A
+git commit -m $1
+git pull origin master --no-edit
+git push origin master:master
+=======
+#!/bin/bash
+git add -A
+git commit -m $1
+git pull origin master --no-edit
+git push origin master:master
+>>>>>>> d95e6b910c8736d2267d5d32a6d1161e09c04834
